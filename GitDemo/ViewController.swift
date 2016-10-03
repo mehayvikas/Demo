bd924fc70f135d26eabb3449c12154b0f7869cec
Vikas Mehay <vikasmehay@vikass-mac-mini.local>
2016-10-03 13:02:39 +0530

    Initial Commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Vikas Mehay on 03/10/16.
+//  Copyright © 2016 Vikas Mehay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
